0bab165441724c770fb505765efd5a5acd532b5b
Ivan Tvisher <Tvisher@mail.ru>
2025-05-22 20:24:57 +0500

    +

--- js/good-works.js
+++ js/good-works.js
@@ -8,3 +8,16 @@ const activitiesList = new Swiper('.activities__list', {
     },
 
 });
+
+
+
+const eventsList = new Swiper('.events-map-list', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    pagination: {
+        el: '.swiper-pagination',
+        type: "bullets",
+        clickable: 1,
+    },
+
+});
